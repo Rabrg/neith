a98f6e4bfdf1d6a2d469f762dc174a5df995cbcc
Ryan Greene <rabrg96@gmail.com>
2017-08-06 15:56:51 -0500

    Added load_dataset to dataset.py

---
+++ neith/dataset.py
@@ -0,0 +1,21 @@
+from os import listdir
+from os.path import isfile, join
+from PIL import Image
+import numpy as np
+
+DATASET_PATH = '../res/'
+CLASS_INDEX = ['(', ')', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'slash', 'star']
+
+
+def load_dataset():
+    files = [f for f in listdir(DATASET_PATH) if isfile(join(DATASET_PATH, f))]
+    features = np.empty((len(files), 32, 32))
+    labels = []
+    for i, file in enumerate(files):
+        feature = np.array(Image.open(DATASET_PATH + file).convert(mode='L'))
+        features[i] = feature
+        labels.append(CLASS_INDEX.index(str(file.split("_")[0])))
+    labels = np.asarray(labels)
+    np.divide(features, 255, out=features)
+    np.subtract(features, 0.5, out=features)
+    return features, labels
