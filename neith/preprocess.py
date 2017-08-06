906b43895d8dcd85d09eb1c9bb42082c449adc63
Ryan Greene <rabrg96@gmail.com>
2017-08-06 18:10:58 -0500

    Refactored remove_overlap_contours in preprocess.py

--- neith/preprocess.py
+++ neith/preprocess.py
@@ -14,26 +14,36 @@ IOU_THRESHOLD = 0
 CONTOUR_LEVEL = 0.8
 
 
+# TODO: optimize from O(n^2) to O(n)
 def remove_overlap_contours(contours):
+    # a list which stores all of the overlapping contours to be removed
     to_remove = []
     for i1, contour1 in enumerate(contours):
         min = np.min(contour1, axis=0)
         max = np.max(contour1, axis=0)
+        # create a bounding box for contour1
         rec1 = Rectangle(min[1], min[0], max[1], max[0])
+        # calculate the area of contour1
         area1 = (rec1[2] - rec1[0]) * (rec1[3] - rec1[1])
         for i2, contour2 in enumerate(contours):
             min = np.min(contour2, axis=0)
             max = np.max(contour2, axis=0)
+            # create a bounding box for contour2
             rec2 = Rectangle(min[1], min[0], max[1], max[0])
+            # calculate the area of contour2
             area2 = (rec2[2] - rec2[0]) * (rec2[3] - rec2[1])
             if contour1 is not contour2:
+                # get the intersect over union for the two bounding boxes
                 iou = get_iou(rec1, rec2)
+                # if the intersect over union is overt the threshold, remove the contour with the smaller area
                 if iou > IOU_THRESHOLD:
                     if area1 > area2 and i2 not in to_remove:
                         to_remove.append(i2)
                     elif i1 not in to_remove:
                         to_remove.append(i1)
+    # a list which stores all of the non overlapping contours
     contours_new = []
+    # populates the new contours list
     for i, c in enumerate(contours):
         if i not in to_remove:
             contours_new.append(c)
