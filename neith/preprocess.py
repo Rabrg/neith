e942226a011715686c69dfe26872e396ac40c0ab
Ryan Greene <rabrg96@gmail.com>
2017-08-06 16:02:32 -0500

    Added get_iou to preprocess.py

--- neith/preprocess.py
+++ neith/preprocess.py
@@ -4,6 +4,10 @@ import operator
 from skimage import measure
 from skimage import transform
 
+from collections import namedtuple
+
+Rectangle = namedtuple('Rectangle', 'xmin ymin xmax ymax')
+
 CONTOUR_LEVEL = 0.8
 
 
@@ -21,3 +25,28 @@ def equation_char_list(r):
     char_imgs = np.asarray([i[1] for i in sorted1])
     np.subtract(char_imgs, 0.5, out=char_imgs)
     return char_imgs
+
+
+def get_iou(rec1, rec2):
+    # determine the coordinates of the intersection rectangle
+    x_left = max(rec1[0], rec2[0])
+    y_top = max(rec1[1], rec2[1])
+    x_right = min(rec1[2], rec2[2])
+    y_bottom = min(rec1[3], rec2[3])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    rec1_area = (rec1[2] - rec1[0]) * (rec1[3] - rec1[1])
+    rec2_area = (rec2[2] - rec2[0]) * (rec2[3] - rec2[1])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(rec1_area + rec2_area - intersection_area)
+    return iou
